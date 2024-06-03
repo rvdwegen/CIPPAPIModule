a13427ca472c1164e5057b9626b0db3b96052c8c
BNWEIN <bweinberg@prime-networks.co.uk>
2024-06-03 22:02:05 +0100

    Added Functions

--- CIPPModule.psd1
+++ CIPPModule.psd1
@@ -85,7 +85,8 @@
         'Get-CIPPApps',
         'Get-CIPPAppStatus',
         'Get-CIPPAutoPilotConfig',
-        'Get-CIPPADConnectStatus'
+        'Get-CIPPADConnectStatus',
+        'Get-CIPPBasicAuth'
     )
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
---
+++ public/Get-CIPPBasicAuth.ps1
@@ -0,0 +1,16 @@
+function Get-CIPPBasicAuth {
+    [CmdletBinding()]
+        Param(
+            [Parameter(Mandatory = $true)]
+            [string]$CustomerTenantID
+        )
+    if ($null -eq $Script:CIPPClientID) {
+        write-error "Cannot continue: CIPP API information not found. Please run Set-CIPPAPIDetails before connecting to the API."
+    }
+    Connect-CIPP -CIPPClientID $script:CIPPClientID -CIPPClientSecret $script:CIPPClientSecret -CIPPAPIUrl $script:CIPPAPIUrl -TenantID $TenantID
+    Write-Host "Getting Basic Auth for customer: $CustomerTenantID" -ForegroundColor Green
+    $BasicAuth = Invoke-RestMethod -Uri "$script:CIPPAPIUrl/api/listbasicauth?tenantfilter=$CustomerTenantID" -Method Get -Headers $script:AuthHeader -ContentType "application/json"
+
+$BasicAuth
+
+}
